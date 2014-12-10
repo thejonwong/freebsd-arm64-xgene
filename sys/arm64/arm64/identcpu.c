a8d2ba63542583027e9f17de8c0257541f8290db
Zbigniew Bodek <zbb@semihalf.com>
2014-12-10 20:45:01 +0100

    Improve ARM64 CPU identification.
    - Keep CPU description per-CPU (indexed by cpuid)
    - Rework Implementer/PartNum relation
      - need to have separate PartNum tables for each implementer

--- sys/arm64/arm64/identcpu.c
+++ sys/arm64/arm64/identcpu.c
@@ -83,38 +83,61 @@ struct cpu_desc {
 	const char	*cpu_part_name;
 };
 
-struct cpu_desc cpu_desc;
-
-struct cpu_implementers {
-	u_int		impl_id;
-	const char	*impl_name;
-};
-
-const struct cpu_implementers cpu_implementers[] = {
-	{ CPU_IMPL_ARM,		"ARM" },
-	{ CPU_IMPL_BROADCOM,	"Broadcom" },
-	{ CPU_IMPL_CAVIUM,	"Cavium" },
-	{ CPU_IMPL_DEC,		"DEC" },
-	{ CPU_IMPL_INFINEON,	"IFX" },
-	{ CPU_IMPL_FREESCALE,	"Freescale" },
-	{ CPU_IMPL_NVIDIA,	"NVIDIA" },
-	{ CPU_IMPL_APM,		"APM" },
-	{ CPU_IMPL_QUALCOMM,	"Qualcomm" },
-	{ CPU_IMPL_MARVELL,	"Marvell" },
-	{ CPU_IMPL_INTEL,	"Intel" },
-	{ 0,			"Unknown Implementer" }
-};
+struct cpu_desc cpu_desc[MAXCPU];
 
 struct cpu_parts {
 	u_int		part_id;
 	const char	*part_name;
 };
+#define	CPU_PART_NONE	{ 0, "Unknown Processor" }
 
-const struct cpu_parts cpu_parts[] = {
-	{ CPU_PART_THUNDER,    "Thunder" },
-	{ CPU_PART_CORTEX_A57, "Cortex-A57" },
-	{ CPU_PART_CORTEX_A53, "Cortex-A53" },
-	{ 0, "Unknown Processor" }
+struct cpu_implementers {
+	u_int			impl_id;
+	const char		*impl_name;
+	/*
+	 * Part number is implementation defined
+	 * so each vendor will have its own set of values and names.
+	 */
+	const struct cpu_parts	*cpu_parts;
+};
+#define	CPU_IMPLEMENTER_NONE	{ 0, "Unknown Implementer", cpu_parts_none }
+
+/*
+ * Per-implementer table of (PartNum, CPU Name) pairs.
+ */
+/* ARM Ltd. */
+static const struct cpu_parts cpu_parts_arm[] = {
+	{ 0xD00, "Cortex-A57" },
+	{ 0xD03, "Cortex-A53" },
+	CPU_PART_NONE,
+};
+/* Cavium */
+static const struct cpu_parts cpu_parts_cavium[] = {
+	{ 0x0A1, "Thunder" },
+	CPU_PART_NONE,
+};
+
+/* Unknown */
+static const struct cpu_parts cpu_parts_none[] = {
+	CPU_PART_NONE,
+};
+
+/*
+ * Implementers table.
+ */
+const struct cpu_implementers cpu_implementers[] = {
+	{ CPU_IMPL_ARM,		"ARM",		cpu_parts_arm },
+	{ CPU_IMPL_BROADCOM,	"Broadcom",	cpu_parts_none },
+	{ CPU_IMPL_CAVIUM,	"Cavium",	cpu_parts_cavium },
+	{ CPU_IMPL_DEC,		"DEC",		cpu_parts_none },
+	{ CPU_IMPL_INFINEON,	"IFX",		cpu_parts_none },
+	{ CPU_IMPL_FREESCALE,	"Freescale",	cpu_parts_none },
+	{ CPU_IMPL_NVIDIA,	"NVIDIA",	cpu_parts_none },
+	{ CPU_IMPL_APM,		"APM",		cpu_parts_none },
+	{ CPU_IMPL_QUALCOMM,	"Qualcomm",	cpu_parts_none },
+	{ CPU_IMPL_MARVELL,	"Marvell",	cpu_parts_none },
+	{ CPU_IMPL_INTEL,	"Intel",	cpu_parts_none },
+	CPU_IMPLEMENTER_NONE,
 };
 
 void identify_cpu(void);
@@ -125,33 +148,37 @@ identify_cpu(void)
 	u_int midr;
 	u_int impl_id;
 	u_int part_id;
+	u_int cpu;
 	uint64_t mpidr;
 	size_t i;
+	const struct cpu_parts *cpu_partsp = NULL;
 
+	cpu = PCPU_GET(cpuid);
 	midr = get_midr();
 
 	impl_id = CPU_IMPL(midr);
 	for (i = 0; i < nitems(cpu_implementers); i++) {
 		if (impl_id == cpu_implementers[i].impl_id ||
 		    cpu_implementers[i].impl_id == 0) {
-			cpu_desc.cpu_impl = impl_id;
-			cpu_desc.cpu_impl_name = cpu_implementers[i].impl_name;
+			cpu_desc[cpu].cpu_impl = impl_id;
+			cpu_desc[cpu].cpu_impl_name = cpu_implementers[i].impl_name;
+			cpu_partsp = cpu_implementers[i].cpu_parts;
 			break;
 		}
 	}
 
 	part_id = CPU_PART(midr);
-	for (i = 0; i < nitems(cpu_parts); i++) {
-		if (part_id == cpu_parts[i].part_id ||
-		    cpu_parts[i].part_id == 0) {
-			cpu_desc.cpu_part_num = part_id;
-			cpu_desc.cpu_part_name = cpu_parts[i].part_name;
+	for (i = 0; &cpu_partsp[i] != NULL; i++) {
+		if (part_id == cpu_partsp[i].part_id ||
+		    cpu_partsp[i].part_id == 0) {
+			cpu_desc[cpu].cpu_part_num = part_id;
+			cpu_desc[cpu].cpu_part_name = cpu_partsp[i].part_name;
 			break;
 		}
 	}
 
-	printf("CPU: %s %s r%dp%d\n", cpu_desc.cpu_impl_name,
-	    cpu_desc.cpu_part_name, CPU_VAR(midr), CPU_REV(midr));
+	printf("CPU: %s %s r%dp%d\n", cpu_desc[cpu].cpu_impl_name,
+	    cpu_desc[cpu].cpu_part_name, CPU_VAR(midr), CPU_REV(midr));
 
 	/*
 	 * Save affinity for the boot CPU.
