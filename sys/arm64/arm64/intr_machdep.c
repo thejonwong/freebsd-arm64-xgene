e383662df5eb810c77e75e5f29f1aad873dfa927
Michal Stanek <mst@semihalf.com>
2015-01-28 17:51:23 +0100

    Store interrupt structures in a linked list instead of array
    IRQ number cannot be used as array index as it can be very high.
    With the linked list, each node stores the physical IRQ number and it can be
    any value in the range of 'int'.

--- sys/arm64/arm64/intr_machdep.c
+++ sys/arm64/arm64/intr_machdep.c
@@ -47,6 +47,7 @@ __FBSDID("$FreeBSD");
 #include <sys/mutex.h>
 #include <sys/cpuset.h>
 #include <sys/interrupt.h>
+#include <sys/queue.h>
 
 #include <machine/cpufunc.h>
 #include <machine/intr.h>
@@ -64,43 +65,46 @@ __FBSDID("$FreeBSD");
 
 static MALLOC_DEFINE(M_INTR, "intr", "Interrupt Services");
 
-struct arm64_intr {
+/*
+ * Linked list of interrupts that have been set-up.
+ * Each element holds the interrupt description
+ * and has to be allocated and freed dynamically.
+ */
+static SLIST_HEAD(, arm64_intr_entry) irq_slist_head =
+    SLIST_HEAD_INITIALIZER(irq_slist_head);
+
+struct arm64_intr_entry {
+	SLIST_ENTRY(arm64_intr_entry) entries;
 	struct intr_event	*i_event;
 
 	enum intr_trigger	i_trig;
 	enum intr_polarity	i_pol;
 
-	u_int			i_irq;		/* Interrupt number */
+	u_int			i_hw_irq;	/* Physical interrupt number */
 	u_int			i_cntidx;	/* Index in intrcnt table */
 	u_int			i_handlers;	/* Allocated handlers */
 	u_long			*i_cntp;	/* Interrupt hit counter */
 };
 
 /* Counts and names for statistics - see sys/sys/interrupt.h */
+/* Tables are indexed by i_cntidx */
 u_long intrcnt[NIRQS];
 char intrnames[NIRQS * INTRNAME_LEN];
 size_t sintrcnt = sizeof(intrcnt);
 size_t sintrnames = sizeof(intrnames);
 
 static u_int intrcntidx;	/* Current index into intrcnt table */
-
-/*
- * Table of interrupts that have been set-up.
- * Each element holds the interrupt description
- * and has to be allocated and freed dynamically.
- */
-static struct arm64_intr *arm64_intrs[NIRQS];
 static u_int arm64_nintrs;	/* Max interrupts number of the root PIC */
 static u_int arm64_nstray;	/* Number of received stray interrupts */
 static device_t root_pic;	/* PIC device for all incoming interrupts */
-static struct mtx intr_table_lock;
 static device_t msi_pic;	/* Device which handles MSI/MSI-X interrupts */
+static struct mtx intr_list_lock;
 
 static void
 intr_init(void *dummy __unused)
 {
 
-	mtx_init(&intr_table_lock, "intr sources lock", NULL, MTX_DEF);
+	mtx_init(&intr_list_lock, "intr sources lock", NULL, MTX_DEF);
 }
 SYSINIT(intr_init, SI_SUB_INTR, SI_ORDER_FIRST, intr_init, NULL);
 
@@ -123,17 +127,23 @@ intrcnt_setname(const char *name, u_int idx)
  * (Similar to ppc's intr_lookup() but without actual
  * lookup since irq number is an index in arm64_intrs[]).
  */
-static struct arm64_intr *
-intr_acquire(u_int irq)
+static struct arm64_intr_entry *
+intr_acquire(u_int hw_irq)
 {
-	struct arm64_intr *intr;
+	struct arm64_intr_entry *intr;
 
-	KASSERT(irq < NIRQS, ("Looking for invalid irq: %d\n", irq));
+	mtx_lock(&intr_list_lock);
 
-	mtx_lock(&intr_table_lock);
-
-	intr = arm64_intrs[irq];
+	SLIST_FOREACH(intr, &irq_slist_head, entries) {
+		if (intr->i_hw_irq == hw_irq) {
+			break;
+		}
+	}
 	if (intr != NULL)
+		goto out;
+
+	/* Do not alloc another intr when max number of IRQs has been reached */
+	if (intrcntidx >= NIRQS)
 		goto out;
 
 	intr = malloc(sizeof(*intr), M_INTR, M_NOWAIT);
@@ -144,37 +154,37 @@ intr_acquire(u_int irq)
 	intr->i_handlers = 0;
 	intr->i_trig = INTR_TRIGGER_CONFORM;
 	intr->i_pol = INTR_POLARITY_CONFORM;
-	intr->i_irq = irq;
 	intr->i_cntidx = atomic_fetchadd_int(&intrcntidx, 1);
 	intr->i_cntp = &intrcnt[intr->i_cntidx];
-	arm64_intrs[irq] = intr;
+	intr->i_hw_irq = hw_irq;
+	SLIST_INSERT_HEAD(&irq_slist_head, intr, entries);
 out:
-	mtx_unlock(&intr_table_lock);
+	mtx_unlock(&intr_list_lock);
 	return intr;
 }
 
 static void
 intr_pre_ithread(void *arg)
 {
-	struct arm64_intr *intr = arg;
+	struct arm64_intr_entry *intr = arg;
 
-	PIC_PRE_ITHREAD(root_pic, intr->i_irq);
+	PIC_PRE_ITHREAD(root_pic, intr->i_hw_irq);
 }
 
 static void
 intr_post_ithread(void *arg)
 {
-	struct arm64_intr *intr = arg;
+	struct arm64_intr_entry *intr = arg;
 
-	PIC_POST_ITHREAD(root_pic, intr->i_irq);
+	PIC_POST_ITHREAD(root_pic, intr->i_hw_irq);
 }
 
 static void
 intr_post_filter(void *arg)
 {
-	struct arm64_intr *intr = arg;
+	struct arm64_intr_entry *intr = arg;
 
-	PIC_POST_FILTER(root_pic, intr->i_irq);
+	PIC_POST_FILTER(root_pic, intr->i_hw_irq);
 }
 
 /*
@@ -256,8 +266,7 @@ arm_map_msix(int irq, uint16_t devid, uint64_t *addr, uint32_t *data)
 int
 arm_enable_intr(void)
 {
-	struct arm64_intr *intr;
-	u_int irq;
+	struct arm64_intr_entry *intr;
 
 	if (root_pic == NULL)
 		panic("Cannot enable interrupts. No PIC configured");
@@ -266,11 +275,7 @@ arm_enable_intr(void)
 	 * Iterate through all possible interrupts and perform
 	 * configuration if the interrupt is registered.
 	 */
-	for (irq = 0; irq < NIRQS; irq++) {
-		intr = arm64_intrs[irq];
-		if (intr == NULL)
-			continue;
-
+	SLIST_FOREACH(intr, &irq_slist_head, entries) {
 		/*
 		 * XXX: In case we allowed to set up interrupt whose number
 		 *	exceeds maximum number of interrupts for the root PIC
@@ -279,25 +284,25 @@ arm_enable_intr(void)
 		 *	This can happen only when calling bus_setup_intr()
 		 *	before the interrupt controller is attached.
 		 */
-		if (intr->i_irq >= arm64_nintrs) {
+		if (intr->i_cntidx >= arm64_nintrs) {
 			/* Better fail when IVARIANTS enabled */
 			KASSERT(0, ("%s: Interrupt %u cannot be handled by the "
 			    "registered PIC. Max interrupt number: %u", __func__,
-			    intr->i_irq, arm64_nintrs - 1));
+			    intr->i_cntidx, arm64_nintrs - 1));
 			/* Print message and disable otherwise */
 			printf("ERROR: Cannot enable irq %u. Disabling.\n",
-			    intr->i_irq);
-			PIC_MASK(root_pic, intr->i_irq);
+			    intr->i_cntidx);
+			PIC_MASK(root_pic, intr->i_hw_irq);
 		}
 
 		if (intr->i_trig != INTR_TRIGGER_CONFORM ||
 		    intr->i_pol != INTR_POLARITY_CONFORM) {
-			PIC_CONFIG(root_pic, intr->i_irq,
+			PIC_CONFIG(root_pic, intr->i_hw_irq,
 			    intr->i_trig, intr->i_pol);
 		}
 
 		if (intr->i_handlers > 0)
-			PIC_UNMASK(root_pic, intr->i_irq);
+			PIC_UNMASK(root_pic, intr->i_hw_irq);
 
 	}
 	/* Enable interrupt reception on this CPU */
@@ -308,27 +313,27 @@ arm_enable_intr(void)
 
 int
 arm_setup_intr(const char *name, driver_filter_t *filt, driver_intr_t handler,
-    void *arg, u_int irq, enum intr_type flags, void **cookiep)
+    void *arg, u_int hw_irq, enum intr_type flags, void **cookiep)
 {
-	struct arm64_intr *intr;
+	struct arm64_intr_entry *intr;
 	int error;
+
+	intr = intr_acquire(hw_irq);
+	if (intr == NULL)
+		return (ENOMEM);
 
 	/*
 	 * Watch out for interrupts' numbers.
 	 * If this is a system boot then don't allow to overfill interrupts
 	 * table (the interrupts will be deconfigured in arm_enable_intr()).
 	 */
-	if (irq >= NIRQS)
+	if (intr->i_cntidx >= NIRQS)
 		return (EINVAL);
-
-	intr = intr_acquire(irq);
-	if (intr == NULL)
-		return (ENOMEM);
 
 	if (intr->i_event == NULL) {
 		error = intr_event_create(&intr->i_event, (void *)intr, 0,
-		    irq, intr_pre_ithread, intr_post_ithread,
-		    intr_post_filter, NULL, "irq%u", irq);
+		    hw_irq, intr_pre_ithread, intr_post_ithread,
+		    intr_post_filter, NULL, "irq%u", hw_irq);
 		if (error)
 			return (error);
 	}
@@ -337,20 +342,20 @@ arm_setup_intr(const char *name, driver_filter_t *filt, driver_intr_t handler,
 	    intr_priority(flags), flags, cookiep);
 
 	if (!error) {
-		mtx_lock(&intr_table_lock);
+		mtx_lock(&intr_list_lock);
 		intrcnt_setname(intr->i_event->ie_fullname, intr->i_cntidx);
 		intr->i_handlers++;
 
 		if (!cold && intr->i_handlers == 1) {
 			if (intr->i_trig != INTR_TRIGGER_CONFORM ||
 			    intr->i_pol != INTR_POLARITY_CONFORM) {
-				PIC_CONFIG(root_pic, intr->i_irq, intr->i_trig,
+				PIC_CONFIG(root_pic, intr->i_hw_irq, intr->i_trig,
 				    intr->i_pol);
 			}
 
-			PIC_UNMASK(root_pic, intr->i_irq);
+			PIC_UNMASK(root_pic, intr->i_hw_irq);
 		}
-		mtx_unlock(&intr_table_lock);
+		mtx_unlock(&intr_list_lock);
 	}
 
 	return (error);
@@ -359,29 +364,29 @@ arm_setup_intr(const char *name, driver_filter_t *filt, driver_intr_t handler,
 int
 arm_teardown_intr(void *cookie)
 {
-	struct arm64_intr *intr;
+	struct arm64_intr_entry *intr;
 	int error;
 
 	intr = intr_handler_source(cookie);
 	error = intr_event_remove_handler(cookie);
 	if (!error) {
-		mtx_lock(&intr_table_lock);
+		mtx_lock(&intr_list_lock);
 		intr->i_handlers--;
 		if (intr->i_handlers == 0)
-			PIC_MASK(root_pic, intr->i_irq);
+			PIC_MASK(root_pic, intr->i_hw_irq);
 		intrcnt_setname(intr->i_event->ie_fullname, intr->i_cntidx);
-		mtx_unlock(&intr_table_lock);
+		mtx_unlock(&intr_list_lock);
 	}
 
 	return (error);
 }
 
 int
-arm_config_intr(u_int irq, enum intr_trigger trig, enum intr_polarity pol)
+arm_config_intr(u_int hw_irq, enum intr_trigger trig, enum intr_polarity pol)
 {
-	struct arm64_intr *intr;
+	struct arm64_intr_entry *intr;
 
-	intr = intr_acquire(irq);
+	intr = intr_acquire(hw_irq);
 	if (intr == NULL)
 		return (ENOMEM);
 
@@ -389,17 +394,22 @@ arm_config_intr(u_int irq, enum intr_trigger trig, enum intr_polarity pol)
 	intr->i_pol = pol;
 
 	if (!cold && root_pic != NULL)
-		PIC_CONFIG(root_pic, intr->i_irq, trig, pol);
+		PIC_CONFIG(root_pic, intr->i_hw_irq, trig, pol);
 
 	return (0);
 }
 
 void
-arm_dispatch_intr(u_int irq, struct trapframe *tf)
+arm_dispatch_intr(u_int hw_irq, struct trapframe *tf)
 {
-	struct arm64_intr *intr;
+	struct arm64_intr_entry *intr;
 
-	intr = arm64_intrs[irq];
+	SLIST_FOREACH(intr, &irq_slist_head, entries) {
+		if (intr->i_hw_irq == hw_irq) {
+			break;
+		}
+	}
+
 	if (intr == NULL)
 		goto stray;
 
@@ -411,7 +421,7 @@ arm_dispatch_intr(u_int irq, struct trapframe *tf)
 stray:
 	if (arm64_nstray < MAX_STRAY_LOG) {
 		arm64_nstray++;
-		printf("Stray IRQ %u\n", irq);
+		printf("Stray IRQ %u\n", hw_irq);
 		if (arm64_nstray >= MAX_STRAY_LOG) {
 			printf("Got %d stray IRQs. Not logging anymore.\n",
 			    MAX_STRAY_LOG);
@@ -419,7 +429,7 @@ stray:
 	}
 
 	if (intr != NULL)
-		PIC_MASK(root_pic, intr->i_irq);
+		PIC_MASK(root_pic, intr->i_hw_irq);
 }
 
 void
