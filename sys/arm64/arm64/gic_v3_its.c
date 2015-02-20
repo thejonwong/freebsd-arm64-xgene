08ed0926755ada645832009ddf681e39b0a87f86
Zbigniew Bodek <zbb@semihalf.com>
2015-02-20 17:06:52 +0100

    Fix share flag value in ITS PENDBASER

--- sys/arm64/arm64/gic_v3_its.c
+++ sys/arm64/arm64/gic_v3_its.c
@@ -705,7 +705,7 @@ lpi_config_cpu(struct gic_v3_its_softc *sc)
 
 	gicr_xbaser = vtophys(pend_base) |
 	    (cache << GICR_PENDBASER_CACHE_SHIFT) |
-	    (cache << GICR_PENDBASER_SHARE_SHIFT);
+	    (share << GICR_PENDBASER_SHARE_SHIFT);
 
 	gic_r_write(gic_sc, 8, GICR_PENDBASER, gicr_xbaser);
 
